875cb9f3dd1209a956f53115f96ff3e469b438e7
Ana Júlia da Silva Roque <a.julia@academico.ifrn.edu.br>
2022-07-26 16:23:19 -0300

    terminando o projeto

---
+++ aula 1 p.d.j/Assets/Scripts/CoinUIController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+
+public class CoinUIController : MonoBehaviour
+{
+    [SerializeField] 
+    private TMP_Text cointext;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
